1cb76fce43e7ace28e6004ebc5e194d7987fbbc7
Alex Rodriguez <unc741@gmail.com>
2018-09-06 00:17:07 -0400

    working...:)

--- new-kali.sh
+++ new-kali.sh
@@ -8,9 +8,6 @@ kaliBaseUrl='https://cdimage.kali.org/'
 # current
 kaliCurrentUrl="${kaliBaseUrl}current/"
 kaliCurrentSHAUrl="${kaliCurrentUrl}${hashAlg}"
-# weekly
-# kaliWeeklyUrl="${kaliBaseUrl}kali-weekly/"
-# kaliWeeklySHAUrl="${kaliWeeklyUrl}${hashAlg}"
 curl='curl -fsSL'
 secretFileFullPath="${HOME}/src/mine/secrets/access_data"
 
@@ -19,20 +16,14 @@ mkdir -p $tmpDir
 # kali stable iso
 $curl $kaliCurrentSHAUrl -o ${tmpDir}/$hashAlg
 $curl "${kaliCurrentSHAUrl}.gpg" -o "${tmpDir}/${hashAlg}.gpg"
-# kali weekly iso
-# $curl $kaliWeeklySHAUrl -o ${tmpDir}/$hashAlg
-# $curl "${kaliWeeklySHAUrl}.gpg" -o "${tmpDir}/${hashAlg}.gpg"
 
 kaliKey=$(wget -q -O - $kaliKeyUrl  | gpg --import 2>&1 | grep key | cut -d ' ' -f 3 | cut -d ':' -f 1 )
 
 gpg --fingerprint $kaliKey
-
 gpg --verify ${tmpDir}/${hashAlg}.gpg ${tmpDir}/$hashAlg
 
 # current
 currentKaliISO=$(curl -s $kaliCurrentUrl | grep -E 'linux-2018.*amd64' | grep -oE 'href.*' | cut -d '"' -f 2)
-# weekly
-# currentKaliISO=$(curl -s $kaliWeeklyUrl | grep -E 'linux-2018-W.*amd64' | grep -oE 'href.*' | cut -d '"' -f 2)
 
 currentHashAlg=$(grep $currentKaliISO ${tmpDir}/$hashAlg | cut -d ' ' -f 1)
 
@@ -61,8 +52,6 @@ fi
 
 # current
 currentKaliISOUrl="${kaliCurrentUrl}${currentKaliISO}"
-# weekly
-# currentKaliISOUrl="${kaliWeeklyUrl}${currentKaliISO}"
 hashAlgOut=$(echo $hashAlg | rev | cut -d 'S' -f 3- | rev | tr '[:upper:]' '[:lower:]')
 
 printf '{"iso_url":"%s","iso_checksum_type":"%s","iso_checksum":"%s","vm_name":"%s","vm_version":"%s","vagrant_cloud_token":"%s"}\n' "$currentKaliISOUrl" "$hashAlgOut" "$currentHashAlg" "$namez" "$vm_version" "$vagrant_cloud_token" | jq . | tee variables.json
