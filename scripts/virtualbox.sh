456710fc1c5e779f5765c89fb19f816c17e6a33d
Alex Rodriguez <alex@secureideas.com>
2019-01-14 11:22:24 -0500

    correcting vbox guest install

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -3,5 +3,5 @@
 # thanks to bento project for this script
 # set a default HOME_DIR environment variable if not set
 echo 'packer' | sudo -S /sbin/reboot -p
-DEBIAN_FRONTEND=noninteractive apt-get install -y virtualbox-guest-x11 --allow -o Dpkg::Options::='--force-confnew'| tee -a $log
+DEBIAN_FRONTEND=noninteractive apt-get install -y virtualbox-guest-x11 -y -o Dpkg::Options::='--force-confnew'| tee -a $log
 # echo 'packer' | sudo -S /sbin/reboot -p
