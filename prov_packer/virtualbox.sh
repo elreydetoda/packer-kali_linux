6229add82652780a9f18989c0bc543e041d3fdee
Alex Rodriguez <alex@secureideas.com>
2019-01-28 00:28:30 -0500

    removing iso and making sure vbox guest installs

--- prov_packer/virtualbox.sh
+++ prov_packer/virtualbox.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -eux
 
 
+# set a default HOME_DIR environment variable if not set
+rm -f /root/*.iso;
 logz='packer-upgrade.log'
 DEBIAN_FRONTEND=noninteractive apt-get install -y virtualbox-guest-x11 -y -o Dpkg::Options::='--force-confnew'| tee -a $logz
 echo 'packer' | sudo -S /sbin/reboot -p
