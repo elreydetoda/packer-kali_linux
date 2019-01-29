b4ca59da9c08a8242b73a926d50241f1cdf42be2
Alex Rodriguez <alex@secureideas.com>
2019-01-29 17:10:46 -0500

    removed, removing guest additions iso

--- prov_packer/virtualbox.sh
+++ prov_packer/virtualbox.sh
@@ -2,7 +2,6 @@
 
 
 # set a default HOME_DIR environment variable if not set
-rm -f /root/*.iso;
 logz='packer-upgrade.log'
 DEBIAN_FRONTEND=noninteractive apt-get install -y virtualbox-guest-x11 -y -o Dpkg::Options::='--force-confnew'| tee -a $logz
 echo 'packer' | sudo -S /sbin/reboot -p
