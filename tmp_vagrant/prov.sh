fec11e36557362076825695ac85dc188de8f8494
alex <10230166+elreydetoda@users.noreply.github.com>
2019-03-12 13:02:01 -0400

    adding enable postgres

--- tmp_vagrant/prov.sh
+++ tmp_vagrant/prov.sh
@@ -2,3 +2,5 @@
 
 # initializing the msf db
 msfdb init
+# https://blog.secureideas.com/2018/09/automating-red-team-homelabs-part-1-kali-automation.html#comment-265
+systemctl enable postgresql.service
