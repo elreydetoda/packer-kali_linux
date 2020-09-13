a32e7b61617459548dd567a7efeca726d65bd743
elreydetoda <alex@secureideas.com>
2020-09-13 11:51:59 -0400

    rename file & remove dumplate commands

--- prov_packer/full-update.sh
+++ prov_packer/full-update.sh
@@ -3,31 +3,6 @@
 # https://elrey.casa/bash/scripting/harden
 set -${-//[s]/}eu${DEBUG+xv}o pipefail
 
-
-function disable_system_misc(){
-
-  # thanks to bento project
-  ########################################
-  # Disable systemd apt timers/services
-  systemctl stop apt-daily.timer;
-  systemctl stop apt-daily-upgrade.timer;
-  systemctl disable apt-daily.timer;
-  systemctl disable apt-daily-upgrade.timer;
-  systemctl mask apt-daily.service;
-  systemctl mask apt-daily-upgrade.service;
-  systemctl daemon-reload;
-
-# Disable periodic activities of apt
-  cat <<EOF >/etc/apt/apt.conf.d/10periodic;
-APT::Periodic::Enable "0";
-APT::Periodic::Update-Package-Lists "0";
-APT::Periodic::Download-Upgradeable-Packages "0";
-APT::Periodic::AutocleanInterval "0";
-APT::Periodic::Unattended-Upgrade "0";
-EOF
-########################################
-}
-
 function update_os(){
 
   ## updating
@@ -46,6 +21,8 @@ function main(){
 
   # establishing a log file variable for the upgrade
   logz='packer-upgrade.log'
+
+  update_os
 
 }
 
