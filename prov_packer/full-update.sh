ce38a199cc4c32162fd1c5ca18064ea2e61d7450
elreydetoda <alex@secureideas.com>
2020-10-21 16:52:59 -0400

    fixing SC2034 shellcheck

--- prov_packer/full-update.sh
+++ prov_packer/full-update.sh
@@ -8,7 +8,6 @@ function update_os() {
   ## updating
   export DEBIAN_FRONTEND=noninteractive
   # from bento project
-  arch="$(uname -r | sed 's/^.*[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\(-[0-9]\{1,2\}\)-//')"
   # fix for old problem of not having the right repos
   # echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' > /etc/apt/sources.list
   # echo 'deb-src http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list
