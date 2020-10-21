8300f62d2c0728fa4b276e0a813b4595c5c1b5d3
elreydetoda <alex@secureideas.com>
2020-10-21 07:25:52 -0400

    pulling from bento project to handle upgrade

--- prov_packer/full-update.sh
+++ prov_packer/full-update.sh
@@ -7,10 +7,15 @@ function update_os() {
 
   ## updating
   export DEBIAN_FRONTEND=noninteractive
+  # from bento project
+  arch="$(uname -r | sed 's/^.*[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\(-[0-9]\{1,2\}\)-//')"
   # fix for old problem of not having the right repos
   # echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' > /etc/apt/sources.list
   # echo 'deb-src http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list
   apt-get update --fix-missing | tee -a $logz
+  # from bento project
+  apt-get -y upgrade -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confnew' "linux-image-${arch}"
+  apt-get -y install linux-headers-"$(uname -r)"
   apt-get upgrade -y -o Dpkg::Options::='--force-confnew' | tee -a $logz
   apt-get dist-upgrade -y -o Dpkg::Options::='--force-confnew' | tee -a $logz
   apt-get autoremove -y -o Dpkg::Options::='--force-confnew' | tee -a $logz
