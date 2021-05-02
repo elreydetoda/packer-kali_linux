5c0228a8ded976c7313dad08a5a916aad1086497
elreydetoda <alex@secureideas.com>
2021-05-02 12:45:12 -0400

    appeasing shfmt

--- ci/scripts/packer_build-wrapper.sh
+++ ci/scripts/packer_build-wrapper.sh
@@ -27,6 +27,7 @@ function packer_build() {
     *)
       # just a stop gap to prevent automated tasks from happening.
       read -rp 'You are about to try and build all the providers at once...are you sure[N/y]' -n 1
+      ;;
   esac
 
 }
@@ -52,6 +53,6 @@ main() {
 }
 
 # https://blog.elreydetoda.site/cool-shell-tricks/#bashscriptingbashsmain
-if [[ "${0}" = "${BASH_SOURCE[0]}" ]]; then
+if [[ "${0}" == "${BASH_SOURCE[0]}" ]]; then
   main "${@}"
 fi
