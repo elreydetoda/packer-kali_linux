3884daf7cf2245b14d66578852a1d507068ab84e
elreydetoda <alex@secureideas.com>
2023-04-10 21:14:00 -0400

    - terraform removing old versions - adding new provider - organizing into terraform folder more

---
+++ ci/terraform/equinix/.terraform.lock.hcl
@@ -0,0 +1,24 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/equinix/equinix" {
+  version     = "1.13.0"
+  constraints = "1.13.0"
+  hashes = [
+    "h1:fdETJXw6MKjGGAnEQVEm9STXlHac/+1fJFek2HVLe68=",
+    "zh:3a0805f40f993918fd7f3095c5f56200e8fdf905ffb5b8a5a114877322156484",
+    "zh:4e17f2db89ffb7f5479915171cdab102d0580b35a5428f53e7b225faa6a60a9e",
+    "zh:595cfe2723315b9c651740f1ced6bf8552ee524915b8bb2f543f202f3dccb216",
+    "zh:6eecfa6fcafee92aee13781d7e9ddd5787895c67cb23834ba0b0f6fbff570612",
+    "zh:82c34da527a9dfe4e5c59cb4cdb6ff6bfae962164b7f28c3b65e16751db32817",
+    "zh:875abc8f118da3d821a4e5fa3c2b0e847a16bb8069e846dfa0775b540d85bcfc",
+    "zh:adb1dd54f0ad92f5f2ab3b37a47da720f50d5449be5ef320bcd57251157be651",
+    "zh:b634d578144f00392a2b16aef82fff718211d04d1d577487db342bc6dc2c0736",
+    "zh:bb157ebb598b296cf2aeb569367bb8a45540bbaf6cb1fcf072ebc818ce5c67ff",
+    "zh:d0bd36e448440a293a222436f894250522ae84c84fe58470666b96a1f6843369",
+    "zh:da60c8d32a365352b19043c7fbd14e70ecc3ed2f31932667b7687d5fc6d8b395",
+    "zh:dbc3cf957f08b69c97d72c2b34a7cd2a774d52b8eacb92cbdb20cf15a402ade8",
+    "zh:f8aa590ad30c823d0bbb0264616c9fa43f708c626b0082341c4f47fa59f8a3f0",
+    "zh:fec98b511c0b457b29dd18b968f445f7f28c7dc09b27eb9160d6b84fdb327498",
+  ]
+}
--- ci/terraform/equinix/main.tf
+++ ci/terraform/equinix/main.tf
@@ -23,9 +23,10 @@ terraform {
 ## Variables that are getting inputted (i.e. variables.tf)
 #   set the PACKET_AUTH_TOKEN env variable for auth
 #   or the auth_token is required
-variable "packet_auth_token" {
+variable "auth_token" {
   description = "The auth token for the account"
   type        = string
+  sensitive   = true
 }
 
 variable "project_id" {
@@ -34,25 +35,11 @@ variable "project_id" {
 }
 
 ##################################################
-# variables that start with v = virtualbox/vmware
-# variables that start with q = qemu
-
-variable "v_server_hostname" {
-  description = "The hostname of the device (server) getting assigned."
-  type        = string
-  default     = "packer-build-box-v"
-}
-
-variable "q_server_hostname" {
-  description = "The hostname of the device (server) getting assigned."
-  type        = string
-  default     = "packer-build-box-qemu"
-}
 
 # NOTE: this needs to be a baremetal host
 #   or else packer won't work
-#   https://www.packet.com/developers/os-compatibility/
-#   https://www.packet.com/developers/api/operatingsystems/
+#   https://deploy.equinix.com/developers/os-compatibility/
+#   https://deploy.equinix.com/developers/api/metal#tag/OperatingSystems
 variable "provision_plan" {
   description = "The type of the device (server) getting assigned."
   type        = string
@@ -62,81 +49,111 @@ variable "provision_plan" {
 ##################################################
 ##  Packet server provisioning
 # defining the packet provider
-provider "metal" {
+provider "equinix" {
   auth_token = var.packet_auth_token
+}
+
+locals {
+  # variables that start with v = virtualbox/vmware
+  # variables that start with q = qemu
+  v_server_hostname = "packer-build-box-v"
+  q_server_hostname = "packer-build-box-qemu"
+  # windows_server_hostname = "packer-build-box-windows"
 }
 
 # querying current ip address, so it is able to only whitelist
 #   this ip address for incoming connections
-data "http" "current_ip" {
-  url = "https://api.ipify.org/?format=json"
-}
+# data "http" "current_ip" {
+#   url = "https://api.ipify.org/?format=json"
+# }
 
 # querying for LTS based on server OS and type
-data "metal_operating_system" "v_ubuntu_lts" {
+data "equinix_metal_operating_system" "ubuntu_lts" {
   distro           = "ubuntu"
-  version          = "20.04"
+  version          = "22.04"
   provisionable_on = var.provision_plan
 }
 
-data "metal_operating_system" "q_ubuntu_lts" {
-  distro           = "ubuntu"
-  version          = "20.04"
-  provisionable_on = var.provision_plan
-}
+# TODO: add windows support
+# data "equinix_metal_operating_system" "windows" {
+#   distro           = "windows"
+#   version          = "2022"
+#   provisionable_on = var.provision_plan
+# }
 
 # provisioning the actual server based on above info
-resource "metal_device" "v_packer_build_server" {
-  hostname         = var.v_server_hostname
+resource "equinix_metal_device" "v_packer_build_server" {
+  hostname         = local.v_server_hostname
   project_id       = var.project_id
-  operating_system = data.metal_operating_system.v_ubuntu_lts.id
+  operating_system = data.metal_operating_system.ubuntu_lts.id
   plan             = var.provision_plan
-  facilities       = ["any"]
+  facilities       = "any"
   billing_cycle    = "hourly"
   tags = [
     "virtualbox-iso", "vmware"
   ]
 
 }
-resource "metal_device" "q_packer_build_server" {
-  hostname         = var.q_server_hostname
+resource "equinix_metal_device" "q_packer_build_server" {
+  hostname         = local.q_server_hostname
   project_id       = var.project_id
-  operating_system = data.metal_operating_system.q_ubuntu_lts.id
+  operating_system = data.metal_operating_system.ubuntu_lts.id
   plan             = var.provision_plan
-  facilities       = ["any"]
+  facilities       = "any"
   billing_cycle    = "hourly"
   tags = [
     "qemu"
   ]
 
 }
+
+# TODO: add windows support
+# resource "equinix_metal_device" "windows_packer_build_server" {
+#   hostname         = local.windows_server_hostname
+#   project_id       = var.project_id
+#   operating_system = data.metal_operating_system.windows.id
+#   plan             = var.provision_plan
+#   facilities       = "any"
+#   billing_cycle    = "hourly"
+#   tags = [
+#     "windows"
+#   ]
+
+# }
+
 ##################################################
 ## Variables that are getting outputted
 # outputing server ip address, so scripts are able
 #   to reference it
 output "v_server_ip" {
-  value = metal_device.v_packer_build_server.access_public_ipv4
+  value = equinix_metal_device.v_packer_build_server.access_public_ipv4
 }
 
 output "q_server_ip" {
-  value = metal_device.q_packer_build_server.access_public_ipv4
+  value = equinix_metal_device.q_packer_build_server.access_public_ipv4
 }
+
+# TODO: add windows support
+# output "windows_server_ip" {
+#   value = equinix_metal_device.windows_packer_build_server.access_public_ipv4
+# }
 
 # outputing your ip address, so scripts are able
 #   to reference it
-output "current_ip" {
-  value = jsondecode(data.http.current_ip.body).ip
-}
+# output "current_ip" {
+#   value = jsondecode(data.http.current_ip.body).ip
+# }
 ##################################################
 
 terraform {
   required_providers {
-    http = {
-      source = "hashicorp/http"
-    }
-    metal = {
-      source = "equinix/metal"
+    # http = {
+    #   source = "hashicorp/http"
+    # }
+    equinix = {
+      source  = "equinix/equinix"
+      version = "1.13.0"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 1.0"
 }
